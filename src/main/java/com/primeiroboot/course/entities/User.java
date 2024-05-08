06422b972cc0f42e99766f6acd1cd4014f05aa48
Davi Gabriel <davig41441503@gmail.com>
2024-05-08 11:05:36 -0300

    h2 database, test profile, jpa

--- src/main/java/com/primeiroboot/course/entities/User.java
+++ src/main/java/com/primeiroboot/course/entities/User.java
@@ -3,8 +3,18 @@ package com.primeiroboot.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user") //renomeia pois User é palavra reservada do H2
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
