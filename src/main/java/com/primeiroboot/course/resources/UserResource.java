2c2c497c471bca54063851e9b5fae626d60d321d
Davi Gabriel <davig41441503@gmail.com>
2024-05-23 15:45:20 -0300

    User insert

--- src/main/java/com/primeiroboot/course/resources/UserResource.java
+++ src/main/java/com/primeiroboot/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.primeiroboot.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.primeiroboot.course.entities.User;
 import com.primeiroboot.course.services.UserService;
@@ -28,5 +32,12 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){//assinatura para o id do parâmetro ser o mesmo do caminho
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping //usado em inserção
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri =  ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
--- src/main/java/com/primeiroboot/course/services/UserService.java
+++ src/main/java/com/primeiroboot/course/services/UserService.java
@@ -24,4 +24,7 @@ public class UserService {
 		return obj.get();
 	}
 	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
